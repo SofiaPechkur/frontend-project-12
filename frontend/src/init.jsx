4f9cfeeac2749f86c660d29e9f6620e5782f2dba
Sofiya <spechkur@mail.ru>
2025-05-18 11:58:06 +0300

    step 11

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -3,9 +3,14 @@ import i18next from 'i18next';
 import { I18nextProvider, initReactI18next } from 'react-i18next';
 import filter from 'leo-profanity';
 import resources from './locales/index.js';
+import { Provider, ErrorBoundary } from '@rollbar/react';
 
 const init = async () => {
     filter.loadDictionary('en');
+    const rollbarConfig = {
+        accessToken: import.meta.env.VITE_ROLLBAR_ACCESS_TOKEN,
+        environment: 'production',
+    };
     const i18n = i18next.createInstance();
         await i18n
             .use(initReactI18next)
@@ -18,9 +23,13 @@ const init = async () => {
             },
         });
     return (
-        <I18nextProvider i18n={i18n}>
-            <App />
-        </I18nextProvider>
+        <Provider config={rollbarConfig}>
+            <ErrorBoundary>
+                <I18nextProvider i18n={i18n}>
+                    <App />
+                </I18nextProvider>
+            </ErrorBoundary>
+        </Provider>
     );
 }
 
